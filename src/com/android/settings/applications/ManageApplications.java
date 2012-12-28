a4422473e8957de05d7930c022b04e585c92fabd
Jimmy <elacuu@gmail.com>
2012-12-28 04:40:16 +0100

    Add fastscroll to the Manage applications screen
    so you don't need to scroll so much if you have alot of apps
    and want to go to the bottom of the list.
    
    Change-Id: I96d892abf53a9da784ae1df056558d4fd3cf64f6

--- src/com/android/settings/applications/ManageApplications.java
+++ src/com/android/settings/applications/ManageApplications.java
@@ -263,6 +263,7 @@ public class ManageApplications extends Fragment implements
                 lv.setSaveEnabled(true);
                 lv.setItemsCanFocus(true);
                 lv.setTextFilterEnabled(true);
+                lv.setFastScrollEnabled(true);
                 mListView = lv;
                 mApplications = new ApplicationsAdapter(mApplicationsState, this, mFilter);
                 mListView.setAdapter(mApplications);
