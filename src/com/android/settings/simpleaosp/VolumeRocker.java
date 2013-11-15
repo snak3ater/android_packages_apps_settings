722a21ec64dc9e5c61e1595306231f95f21cde6a
BigBrother1984 <stevewatersy@gmail.com>
2013-11-15 16:51:05 +0100

    Volume Rocker: Hardcode music track seek option (2/2).
    * The feature is enabled by default but not configurable.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>

--- src/com/android/settings/simpleaosp/VolumeRocker.java
+++ src/com/android/settings/simpleaosp/VolumeRocker.java
@@ -14,11 +14,9 @@ import com.android.settings.SettingsPreferenceFragment;
 public class VolumeRocker extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String KEY_VOLUME_WAKE = "pref_volume_wake";
-    private static final String KEY_VOLBTN_MUSIC_CTRL = "volbtn_music_controls";
     private static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
     private CheckBoxPreference mVolumeWake;
-    private CheckBoxPreference mVolBtnMusicCtrl;
     private ListPreference mVolumeKeyCursorControl;
 
     @Override
@@ -30,10 +28,6 @@ public class VolumeRocker extends SettingsPreferenceFragment implements OnPrefer
         mVolumeWake = (CheckBoxPreference) findPreference(KEY_VOLUME_WAKE);
         mVolumeWake.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
                     Settings.System.VOLUME_WAKE_SCREEN, 0) == 1);
-
-        mVolBtnMusicCtrl = (CheckBoxPreference) findPreference(KEY_VOLBTN_MUSIC_CTRL);
-        mVolBtnMusicCtrl.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
-                   Settings.System.VOLBTN_MUSIC_CONTROLS, 0) == 1);
 
         mVolumeKeyCursorControl = (ListPreference) findPreference(VOLUME_KEY_CURSOR_CONTROL);
         if(mVolumeKeyCursorControl != null) {
@@ -50,11 +44,6 @@ public class VolumeRocker extends SettingsPreferenceFragment implements OnPrefer
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.VOLUME_WAKE_SCREEN,
                     mVolumeWake.isChecked()
-                    ? 1 : 0);
-         } else if (preference == mVolBtnMusicCtrl) {
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.VOLBTN_MUSIC_CONTROLS,
-                    mVolBtnMusicCtrl.isChecked()
                     ? 1 : 0);
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
