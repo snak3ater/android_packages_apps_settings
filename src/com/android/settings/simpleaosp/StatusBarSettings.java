31daab4899991e7bbb57ad4e578e17ae6437c221
Jon Haus <haus.xda@gmail.com>
2014-01-05 01:04:15 -0500

    [2/2] Settings: Update network traffic indicator
    *Update settings option to cycle through display modes Off->bit/s->Bytes/s
    
    *Minor formatting changes
    
    Change-Id: I9add610a8d14f5a48f2af0a82ced0c186224e282
    
    Conflicts:
    
    	res/values/custom_strings.xml
    	src/org/omnirom/omnigears/interfacesettings/BarsSettings.java
    
    Conflicts:
    	res/values/mahdi_strings.xml
    	res/xml/status_bar.xml
    	src/com/android/settings/mahdi/StatusBar.java

--- src/com/android/settings/simpleaosp/StatusBarSettings.java
+++ src/com/android/settings/simpleaosp/StatusBarSettings.java
@@ -24,12 +24,14 @@ OnPreferenceChangeListener {
     private static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
     // Notification Count
     private static final String STATUSBAR_NOTIF_COUNT = "status_bar_notif_count";
+    private static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
 
     private ListPreference mQuickPulldown;
     private CheckBoxPreference mStatusBarBrightnessControl;
     // Double-tap to sleep
     private CheckBoxPreference mStatusBarDoubleTapSleepGesture;
     private CheckBoxPreference mStatusBarNotifCount;
+    private CheckBoxPreference mStatusBarTraffic;
 
 
     @Override
@@ -46,6 +48,12 @@ OnPreferenceChangeListener {
             mStatusBarDoubleTapSleepGesture = (CheckBoxPreference) getPreferenceScreen().findPreference(DOUBLE_TAP_SLEEP_GESTURE);
             mStatusBarDoubleTapSleepGesture.setChecked((Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 0) == 1));
+
+	mStatusBarTraffic = (CheckBoxPreference) prefSet.findPreference(STATUS_BAR_TRAFFIC);
+        int intState = Settings.System.getInt(resolver, Settings.System.STATUS_BAR_TRAFFIC, 0);
+        intState = setStatusBarTrafficSummary(intState);
+        mStatusBarTraffic.setChecked(intState > 0);
+        mStatusBarTraffic.setOnPreferenceChangeListener(this);
 
         // Quick Settings pull down
         mQuickPulldown = (ListPreference) getPreferenceScreen().findPreference(QUICK_PULLDOWN);
@@ -88,8 +96,18 @@ OnPreferenceChangeListener {
                     Settings.System.QS_QUICK_PULLDOWN, quickPulldownValue);
             mQuickPulldown.setSummary(mQuickPulldown.getEntries()[quickPulldownIndex]);
             return true;
+       } else if (preference == mStatusBarTraffic) {
+
+            // Increment the state and then update the label
+            int intState = Settings.System.getInt(resolver, Settings.System.STATUS_BAR_TRAFFIC, 0);
+            intState++;
+            intState = setStatusBarTrafficSummary(intState);
+            Settings.System.putInt(resolver, Settings.System.STATUS_BAR_TRAFFIC, intState);
+            if (intState > 1) {return false;}        
+        } else {
+            return false;
         }
-        return false;
+        return true;
     }
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
@@ -111,5 +129,18 @@ OnPreferenceChangeListener {
     @Override
     public void onResume() {
         super.onResume();
+    }
+
+	private int setStatusBarTrafficSummary(int intState) {
+        // These states must match com.android.systemui.statusbar.policy.Traffic
+        if (intState == 1) {
+            mStatusBarTraffic.setSummary(R.string.show_network_speed_bits);
+        } else if (intState == 2) {
+            mStatusBarTraffic.setSummary(R.string.show_network_speed_bytes);
+        } else {
+            mStatusBarTraffic.setSummary(R.string.show_network_speed_summary);
+            return 0;
+        }
+        return intState;
     }
 }
