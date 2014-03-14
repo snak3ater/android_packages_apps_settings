200231df75d33a1ad3f558f59fbb894002405a14
AndroidRul3z <ferrettidario@gmail.com>
2014-03-14 15:23:50 +0100

    Navbar Custom Glow Time (2/2)
    Conflicts:
    	res/values/purity_strings.xml
    	res/xml/navbar_settings.xml
    	src/com/android/settings/purity/NavbarSettings.java
    
    Conflicts:
    	res/values/simpleaosp_strings.xml

--- src/com/android/settings/simpleaosp/NavigationBarSettings.java
+++ src/com/android/settings/simpleaosp/NavigationBarSettings.java
@@ -16,14 +16,22 @@ OnPreferenceChangeListener {
 
     private static final String KEY_NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
     private static final String KEY_NAVIGATION_BAR_GENERAL_CATEGORY = "navigation_bar_general_category";
+    private static final String NAVIGATION_BUTTON_GLOW_TIME = "navigation_button_glow_time";
+
 
     private SeekBarPreference mNavigationBarHeight;
+    private SeekBarPreference mNavigationButtonGlowTime;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.navigation_bar_settings);
+
+	mNavigationButtonGlowTime = (SeekBarPreference) findPreference(NAVIGATION_BUTTON_GLOW_TIME);
+        mNavigationButtonGlowTime.setProgress(Settings.System.getInt(getContentResolver(),
+                  Settings.System.NAVIGATION_BUTTON_GLOW_TIME, 500));
+        mNavigationButtonGlowTime.setOnPreferenceChangeListener(this);
 
         mNavigationBarHeight = (SeekBarPreference) findPreference(KEY_NAVIGATION_BAR_HEIGHT);
         mNavigationBarHeight.setProgress((int)(Settings.System.getFloat(getContentResolver(),
@@ -33,11 +41,16 @@ OnPreferenceChangeListener {
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mNavigationBarHeight) {
+	if (preference == mNavigationButtonGlowTime) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.NAVIGATION_BUTTON_GLOW_TIME, (Integer)objValue);
+            return true;
+       } else if (preference == mNavigationBarHeight) {
             Settings.System.putFloat(getActivity().getContentResolver(),
                     Settings.System.NAVIGATION_BAR_HEIGHT, (Integer)objValue / 100f);
             mNavigationBarHeight.setTitle(getResources().getText(R.string.navigation_bar_height) + " " + (Integer)objValue + "%");
-        }
         return true;
+        }
+        return false;
     }
 }
