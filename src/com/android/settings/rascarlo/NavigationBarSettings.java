59838b8865a0361fa772ef141b77c3041ac5da32
snak3ater <saeedyasir91@gmail.com>
2014-06-09 01:30:31 +0500

    Reimplement Navbar Height

--- src/com/android/settings/rascarlo/NavigationBarSettings.java
+++ src/com/android/settings/rascarlo/NavigationBarSettings.java
@@ -7,7 +7,7 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
-
+import android.preference.SeekBarPreference;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import android.preference.PreferenceFragment;
@@ -20,7 +20,7 @@ OnPreferenceChangeListener {
     private static final String KEY_NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
 private static final String NAVBAR_BUTTON_TINT = "navbar_button_tint";
 
-    private ListPreference mNavigationBarHeight;
+   private SeekBarPreference mNavigationBarHeight;
     private ColorPickerPreference mNavbarButtonTint;
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,16 +28,14 @@ private static final String NAVBAR_BUTTON_TINT = "navbar_button_tint";
 
         addPreferencesFromResource(R.xml.navigation_bar_settings);
 
-        mNavigationBarHeight = (ListPreference) findPreference(KEY_NAVIGATION_BAR_HEIGHT);
+        mNavigationBarHeight = (SeekBarPreference) findPreference(KEY_NAVIGATION_BAR_HEIGHT);
+        mNavigationBarHeight.setProgress((int)(Settings.System.getFloat(getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_HEIGHT, 1f) * 100));
+        mNavigationBarHeight.setTitle(getResources().getText(R.string.navigation_bar_height) + " " + mNavigationBarHeight.getProgress() + "%");
         mNavigationBarHeight.setOnPreferenceChangeListener(this);
-        int statusNavigationBarHeight = Settings.System.getInt(getActivity().getApplicationContext()
-                .getContentResolver(),
-                Settings.System.NAVIGATION_BAR_HEIGHT, 48);
-        mNavigationBarHeight.setValue(String.valueOf(statusNavigationBarHeight));
-        mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntry());
 
 
-mNavbarButtonTint = (ColorPickerPreference) findPreference(NAVBAR_BUTTON_TINT);
+	mNavbarButtonTint = (ColorPickerPreference) findPreference(NAVBAR_BUTTON_TINT);
         mNavbarButtonTint.setOnPreferenceChangeListener(this);
         int intColor = Settings.System.getInt(getActivity().getContentResolver(),
         Settings.System.NAVIGATION_BAR_TINT, 0xffffffff);
@@ -48,12 +46,11 @@ mNavbarButtonTint = (ColorPickerPreference) findPreference(NAVBAR_BUTTON_TINT);
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         if (preference == mNavigationBarHeight) {
-            int statusNavigationBarHeight = Integer.valueOf((String) objValue);
-            int index = mNavigationBarHeight.findIndexOfValue((String) objValue);
-            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
-                    Settings.System.NAVIGATION_BAR_HEIGHT, statusNavigationBarHeight);
-            mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntries()[index]);
+            Settings.System.putFloat(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_HEIGHT, (Integer)objValue / 100f);
+            mNavigationBarHeight.setTitle(getResources().getText(R.string.navigation_bar_height) + " " + (Integer)objValue + "%");
         return true;
+
    }else if (preference == mNavbarButtonTint) {
 
             String hex = ColorPickerPreference.convertToARGB(
