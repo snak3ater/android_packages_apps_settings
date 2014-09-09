f8d16fd3d30215b4e2634abf30b60d45d950efbe
MrBaNkS <banksmi09@gmail.com>
2014-09-09 11:28:50 -0500

    Remove SimpleAOSP build version. Will revisit another time maybe.
    Squashed commit of the following:
    
    commit 48d38cf34757e044d4949c17c053d461726ef89c
    Author: MrBaNkS <banksmi09@gmail.com>
    Date:   Tue Sep 9 11:28:19 2014 -0500
    
        Revert "Add SimpleAOSP build version based on ro.delta.version"
    
        This reverts commit 8983735cb25c9dec8df6c60b557d6e6e562ce91f.
    
    commit 7d9651b06b8d7f9759797afdc50e9ed090966aef
    Author: MrBaNkS <banksmi09@gmail.com>
    Date:   Tue Sep 9 11:28:00 2014 -0500
    
        Revert "Should have used Unknown"
    
        This reverts commit 8e846c13f786d3cfa5207230765226173bfedd3f.
    
    commit 444194c942213d0ba246347118d66745cb4c3e74
    Author: MrBaNkS <banksmi09@gmail.com>
    Date:   Tue Sep 9 11:27:42 2014 -0500
    
        Remove French SimpleAOSP build translation

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -64,7 +64,6 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
     private static final String KEY_EQUIPMENT_ID = "fcc_equipment_id";
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_MOD_BUILD_DATE = "build_date";
-    private static final String KEY_SIMPLEAOSP_VERSION = "simpleaosp_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -96,7 +95,6 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
-        setValueSummary(KEY_SIMPLEAOSP_VERSION, "ro.delta.version");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
