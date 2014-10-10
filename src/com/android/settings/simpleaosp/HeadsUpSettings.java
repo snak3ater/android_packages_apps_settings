7cb95a03901e7f6d96941a560671a7a7100a0095
snak3ater <saeedyasir91@gmail.com>
2014-10-10 16:26:30 +0500

    Revert: Show headsup at bottom [2/2]

--- src/com/android/settings/simpleaosp/HeadsUpSettings.java
+++ src/com/android/settings/simpleaosp/HeadsUpSettings.java
@@ -32,14 +32,12 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     private static final String PREF_HEADS_UP_SNOOZE_TIME = "heads_up_snooze_time";
     private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
     private static final String PREF_HEADS_UP_SHOW_UPDATE = "heads_up_show_update";
-    private static final String PREF_HEADS_UP_GRAVITY = "heads_up_gravity";
     private static final String PREF_HEADS_UP_EXCLUDE_FROM_LOCK_SCREEN = "heads_up_exclude_from_lock_screen";
     
     ListPreference mHeadsUpSnoozeTime;
     ListPreference mHeadsUpTimeOut;
     CheckBoxPreference mHeadsUpExpanded;
     CheckBoxPreference mHeadsUpShowUpdates;
-    CheckBoxPreference mHeadsUpGravity;
     CheckBoxPreference mHeadsExcludeFromLockscreen;
 
     @Override
@@ -61,11 +59,6 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
         mHeadsUpShowUpdates.setChecked(Settings.System.getIntForUser(getContentResolver(),
                 Settings.System.HEADS_UP_SHOW_UPDATE, 0, UserHandle.USER_CURRENT) == 1);
         mHeadsUpShowUpdates.setOnPreferenceChangeListener(this);
-
-        mHeadsUpGravity = (CheckBoxPreference) findPreference(PREF_HEADS_UP_GRAVITY);
-        mHeadsUpGravity.setChecked(Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.HEADS_UP_GRAVITY_BOTTOM, 0, UserHandle.USER_CURRENT) == 1);
-        mHeadsUpGravity.setOnPreferenceChangeListener(this);
 
         mHeadsExcludeFromLockscreen = (CheckBoxPreference) findPreference(PREF_HEADS_UP_EXCLUDE_FROM_LOCK_SCREEN);
         mHeadsExcludeFromLockscreen.setChecked(Settings.System.getIntForUser(getContentResolver(),
@@ -126,11 +119,6 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
         } else if (preference == mHeadsUpShowUpdates) {
             Settings.System.putIntForUser(getContentResolver(),
                     Settings.System.HEADS_UP_SHOW_UPDATE,
-                    (Boolean) newValue ? 1 : 0, UserHandle.USER_CURRENT);
-            return true;
-        } else if (preference == mHeadsUpGravity) {
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.HEADS_UP_GRAVITY_BOTTOM,
                     (Boolean) newValue ? 1 : 0, UserHandle.USER_CURRENT);
             return true;
         } else if (preference == mHeadsExcludeFromLockscreen) {
